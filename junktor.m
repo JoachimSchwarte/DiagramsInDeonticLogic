f17df46834f2e3b41046b61d372a84064d6893ab
schwarte <schwarte@IWBLAPTOP019.IWB.local>
2021-12-17 15:07:45 +0100

    junktor.m kommentiert

--- junktor.m
+++ junktor.m
@@ -1,4 +1,13 @@
 function [C] = junktor(J,A,B)
+% J ist eine ganze Zahl im Bereich 0 bis 15
+% und bezeichnet einen der 16 möglichen aussagenlogischen
+% Junktoren.
+% A und B sind Vektoren, die gleiche Länge haben und
+% Wahrheitswerte beinhalten.
+% C ist ein Wahrheitswertvektor, der die gleiche Länge hat
+% wie die Vektoren A und B. Die Wahrheitswertverteilung in 
+% C ergibt sich durch die Anwendung des Junktor J auf die
+% Vektoren A und B.
     J1 = floor(J/8);
     J=J-J1*8;
     J2 = floor(J/4);
